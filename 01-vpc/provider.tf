71d0ad1ea83f70732be475d864460e0f0247ae91
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-10-17 11:10:26 +0530

    main

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "guru97s-remote-state"
-    key    = "guru97s-locking-expense-infra-dev-vpc-main"
+    bucket = "guru97s-remote-vpc"
+    key    = "guru97s-locking-expense-infra-dev-vpc"
     region = "us-east-1"
     dynamodb_table = "guru97s-locking-expense-terraform-dev"
   }
