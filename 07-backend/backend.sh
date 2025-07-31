c6786223203b23426d79c5a1e9b499c17fd9a26b
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-07-31 11:42:24 +0530

    adding comments

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -2,5 +2,5 @@
 component=$1
 environment=$2
 dnf install ansible -y
-pip3.9 install botocore boto3
+pip3.9 install botocore boto3 #to connect to AWS  we need botocore,boto3
 ansible-pull -i localhost, -U https://github.com/guru-sulakhe/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
