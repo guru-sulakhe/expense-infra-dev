e21dce2c3ba939732068d9dc60082b5a2b2f6b8b
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-07-31 14:12:00 +0530

    adding comments

--- 07-backend/main.tf
+++ 07-backend/main.tf
@@ -95,7 +95,7 @@ resource "null_resource" "backend_delete" {
 # creating target group 
 resource "aws_lb_target_group" "backend" {
   name     = "${var.project_name}-${var.environment}-${var.common_tags.Component}" #TargetGroup Name
-  port     = 8080
+  port     = 8080 #TargetGroup Port 
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
 
