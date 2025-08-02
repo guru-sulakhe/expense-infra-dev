92dd3d35e24c5c029899ee51a5d4232b1bba0681
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-08-02 17:51:00 +0530

    adding comments

--- 07.1-acm/main.tf
+++ 07.1-acm/main.tf
@@ -11,7 +11,7 @@ resource "aws_acm_certificate" "expense" {
   )
 }
 
-resource "aws_route53_record" "expense" {
+resource "aws_route53_record" "expense" { #this will create a R53 record for the aws_acm_certificate.expense
   for_each = {
     for dvo in aws_acm_certificate.expense.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -25,10 +25,10 @@ resource "aws_route53_record" "expense" {
   records         = [each.value.record]
   ttl             = 1
   type            = each.value.type
-  zone_id         = var.zone_id
+  zone_id         = var.zone_id #Your AWS Hosted zone_id
 }
 
-resource "aws_acm_certificate_validation" "example" {
+resource "aws_acm_certificate_validation" "example" { #This will validates the aws_acm_certificate.expense
   certificate_arn         = aws_acm_certificate.expense.arn
   validation_record_fqdns = [for record in aws_route53_record.expense : record.fqdn]
 }
